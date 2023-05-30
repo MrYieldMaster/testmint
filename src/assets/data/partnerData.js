feb5730003a8043824d6e9d8d88d4a32fd6d0ac3
Anthony Cox <double_a1420@yahoo.com.com>
2023-05-29 17:33:56 -0700

    commit

--- src/assets/data/partnerData.js
+++ src/assets/data/partnerData.js
@@ -1,17 +1,17 @@
-import clientThumb1 from "assets/images/partners/1.png";
-import clientThumb2 from "assets/images/partners/2.png";
-import clientThumb3 from "assets/images/partners/3.png";
-import clientThumb4 from "assets/images/partners/4.png";
-import clientThumb5 from "assets/images/partners/5.png";
-import clientThumb6 from "assets/images/partners/6.png";
-import clientThumb7 from "assets/images/partners/7.png";
-import clientThumb8 from "assets/images/partners/8.png";
+import clientThumb1 from "assets/images/partners/11.png";
+import clientThumb2 from "assets/images/partners/12.png";
+import clientThumb3 from "assets/images/partners/13.png";
+import clientThumb4 from "assets/images/partners/14.png";
+import clientThumb5 from "assets/images/partners/15.png";
+import clientThumb6 from "assets/images/partners/16.png";
+import clientThumb7 from "assets/images/partners/17.png";
+import clientThumb8 from "assets/images/partners/18.png";
 
 const data = [
   clientThumb1,
   clientThumb2,
   clientThumb3,
-  clientThumb4,
+  clientThumb4, 
   clientThumb5,
   clientThumb6,
   clientThumb7,
